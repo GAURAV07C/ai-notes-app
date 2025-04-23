51d7dbd481d6889b9c5ece39eead06a19a80260e
Gaurav kumar <gaurav989267@gmail.com>
2025-04-23 14:31:19 +0530

    Add theme provider and update layout for dark mode support; include next-themes package

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import FeaturesSection from "@/components/Home/features";
 import CtaSection from "@/components/Home/cta-section";
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-white dark:bg-gray-950">
+    <div className="flex flex-col min-h-screen bg-white dark:bg-gray-950 ">
       <NavBar />
 
       <main className="flex-1">
